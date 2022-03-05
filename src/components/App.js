3dda0cee9b9c2ee38ae08f692585eef4e2143c76
aman2222kumar <42084981+aman2222kumar@users.noreply.github.com>
2022-03-05 17:15:28 +0530

    initial

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,25 @@
-import React, {Component, useState} from "react";
+import React, { Component, useState } from "react";
 import "./../styles/App.css";
 
 function App() {
+  const [id, setId] = useState("text");
+
+  function PrintTag() {
+    setId("para");
+  }
   return (
     <div id="main">
-      // Do not alter the main div
+      <button onClick={PrintTag} id={id}>
+        Click me
+      </button>
+
+      <p>
+        {id == "para"
+          ? "Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy"
+          : " "}
+      </p>
     </div>
   );
 }
-
 
 export default App;
